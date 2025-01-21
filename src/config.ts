f5b41413ca74e02ad258361b507d9e6edf39ee6e
HaeWon - Jung <62277506+Jung-Haewon@users.noreply.github.com>
2025-01-21 14:56:26 +0900

    fix : 오타 수정

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.DEV
 ? 'http://localhost:8080/api'
-: 'http://http://54.234.229.182:8080/api'
+: 'http://54.234.229.182:8080/api'
 
 export const API = {
     JOIN: `${BASE_URL}/join`,
